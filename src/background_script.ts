d0b695475ee9c1a20851dfc5995ba99cbfd73e86
Stephen L. <LRQ3000@gmail.com>
2025-02-08 19:00:57 +0100

    first attempt at migrating to MV3
    Signed-off-by: Stephen L. <LRQ3000@gmail.com>

--- src/background_script.ts
+++ src/background_script.ts
@@ -1,67 +1,77 @@
-import {turndownServie, copyToClipboard} from './types'
-
-const sendMessageToTab = () => {
-  console.log('Fetching selected html')
-  chrome.tabs.query({active: true}, (tabs) => {
-    if (tabs[0]) {
-      chrome.tabs.executeScript(tabs[0].id as number, {file: 'js/content_script_get_selection.bundle.js'})
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['js/content_script_get_selection.bundle.js']
+  }, function (selection) {
+    if (selection && selection[0]) {
+      const markdownText = selection[0];
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['js/content_script_show_message.bundle.js']
+      }, function () {
+        navigator.clipboard.writeText(markdownText).then(function() {
+          console.log('Text copied to clipboard');
+        }).catch(function(err) {
+          console.error('Failed to copy text: ', err);
+        });
+      });
     }
-    else {
-      console.log('No active tab')
-    }
-  })
-}
-const onContextMenuClicked = (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
-  if (info.menuItemId === 'copy-as-markdown') {
-    sendMessageToTab()
-  }
-}
-
-const onCommandTriggered = (command: string) => {
-  if (command === 'copy-as-markdown') {
-    sendMessageToTab()
-  }
-}
-
-const onBrowserActionClicked = () => {
-  sendMessageToTab()
-}
-
-const onMessageReceived = (message: {selection?: string},
-  sender: chrome.runtime.MessageSender,
-  sendResponse: (response?: any) => void) => {
-  if (message && message.selection) {
-    const markdown = turndownServie.turndown(message.selection)
-    copyToClipboard(markdown)
-    console.log('Copied')
-    console.log(markdown)
-    sendResponse(true)
-    if (sender.tab) {
-      chrome.tabs.executeScript(sender.tab.id as number, {file: 'js/content_script_show_message.bundle.js'})
-    }
-  } else {
-    console.log('There is no selection.')
-    sendResponse(false)
-  }
-}
-
-const initBackgroundScript = () => {
-  console.log('background running');
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: 'copy-as-markdown',
-      title: 'Copy as Markdown',
-      contexts: ['selection'],
-    })
   });
+});
 
-  chrome.contextMenus.onClicked.addListener(onContextMenuClicked)
 
-  chrome.commands.onCommand.addListener(onCommandTriggered);
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'copy-as-markdown') {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      const tab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['js/content_script_get_selection.bundle.js']
+      }, function (selection) {
+        if (selection && selection[0]) {
+          const markdownText = selection[0];
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['js/content_script_show_message.bundle.js']
+          }, function () {
+            navigator.clipboard.writeText(markdownText).then(function() {
+              console.log('Text copied to clipboard');
+            }).catch(function(err) {
+              console.error('Failed to copy text: ', err);
+            });
+          });
+        }
+      });
+    });
+  }
+});
 
-  chrome.runtime.onMessage.addListener(onMessageReceived)
 
-  chrome.browserAction.onClicked.addListener(onBrowserActionClicked)
-}
+chrome.contextMenus.create({
+  id: 'copy-as-markdown-context-menu',
+  title: 'Copy as Markdown',
+  contexts: ['selection']
+});
 
-initBackgroundScript();
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === 'copy-as-markdown-context-menu') {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['js/content_script_get_selection.bundle.js']
+    }, function (selection) {
+      if (selection && selection[0]) {
+        const markdownText = selection[0];
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ['js/content_script_show_message.bundle.js']
+        }, function () {
+          navigator.clipboard.writeText(markdownText).then(function() {
+              console.log('Text copied to clipboard');
+          }).catch(function(err) {
+              console.error('Failed to copy text: ', err);
+          });
+        });
+      }
+    });
+  }
+});
